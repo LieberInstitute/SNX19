30f58411da679836922cd0bc0caeae77a4ac219e
aprice26 <amanda.joy.price@gmail.com>
2017-04-12 17:18:23 -0400

    updating the files

--- code/Amanda/code/get_seq_around_SNP.R
+++ code/Amanda/code/get_seq_around_SNP.R
@@ -1,28 +1,24 @@
 library('BSgenome.Hsapiens.UCSC.hg19')
 library(readxl)
 
-table = read_excel("./Dropbox/SNX19/Junc8_10_SNX19_eQTL_results_updatedSNPs.xlsx")
-table = table[which(table$Set!="na"),]
-dim(table)
-table = table[,c("updated_newRef","updated_newCount","updated_SNP","snpChrPos","UniqueID",
-                 "snps","All_pvalue","PGC2014_P" ,"PGC2016_P","MAF_All","Set")]
-table = table[which(table$updated_SNP!="rs67215852"),]
-table[,c("updated_SNP","snpChrPos")]
-ids = as.numeric(gsub("chr11:", "", table$snpChrPos, fixed=T))
-ids
+table = read_excel("./Dropbox/SNX19/code/Amanda/supporting_files/SCZ52_updated_pgc_vals_marquee_junction_eqtl_040517_CUR.xlsx", sheet=2)
+table = data.frame(table)
 
+alleles = strsplit(table$observed, "/", fixed=T)
+allele1 = allele2 = c()
+for (i in 1:length(alleles)){allele1[i] = alleles[[i]][2]}
+for (i in 1:length(alleles)){allele2[i] = alleles[[i]][1]}
+pos = table$chromEnd
 chr = 'chr11'
-ref = table$updated_newRef
-alt = table$updated_newCount
-offset = 15
+offset = 10
 
-RefSeq = paste0(">Set",table$Set,"_", table$updated_SNP,"\n",
-                getSeq(Hsapiens,chr,ids-offset,ids-1),
-                ref,
-                getSeq(Hsapiens,chr,ids+1,ids+offset))
-AltSeq = paste0(">Set",table$Set,"_", table$updated_SNP,"\n",
-                getSeq(Hsapiens,chr,ids-offset,ids-1),
-                alt,
-                getSeq(Hsapiens,chr,ids+1,ids+offset))
-write.table(RefSeq, "./Dropbox/SNX19/ref.fa", quote = F, row.names=F, col.names=F)
-write.table(AltSeq, "./Dropbox/SNX19/alt.fa", quote = F, row.names=F, col.names=F)
+RefSeq = paste0(">Extended_Hap_", table$name,"\n",
+                getSeq(Hsapiens,chr,pos-offset,pos-1),
+                allele1,
+                getSeq(Hsapiens,chr,pos+1,pos+offset))
+AltSeq = paste0(">Extended_Hap_",table$Set,"_", table$updated_SNP,"\n",
+                getSeq(Hsapiens,chr,pos-offset,pos-1),
+                allele2,
+                getSeq(Hsapiens,chr,pos+1,pos+offset))
+write.table(RefSeq, "./Dropbox/SNX19/code/Amanda/supporting_files/ref_extended_Hap.fa", quote = F, row.names=F, col.names=F)
+write.table(AltSeq, "./Dropbox/SNX19/code/Amanda/supporting_files/alt_extended_Hap.fa", quote = F, row.names=F, col.names=F)
